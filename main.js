587a5880f107ef3278deb02f52e92ba0ada151aa
Andy Pan <andy0130tw@yahoo.com.tw>
2026-06-09 00:20:10 +0800

    prettify

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ function hexify(a) {
 }
 
 function hexify4(a) {
-    return (Array(5-a.toString(16).length).join('0') + a.toString(16))
+    return (Array(5 - a.toString(16).length).join('0') + a.toString(16))
 }
 
 function binify(a) {
     out = []
     for (let n of a) {
-        out.push(Array(9-n.toString(2).length).join('0') + n.toString(2))
+        out.push(Array(9 - n.toString(2).length).join('0') + n.toString(2))
     }
     return out.join(' ')
 }
@@ -48,13 +48,14 @@ function bitifybyte(a) {
     //        (a >> 2) & 1,
     //         a & 1]
     return [a & 1,
-           (a >> 1) & 1,
-           (a >> 2) & 1,
-           (a >> 3) & 1,
-           (a >> 4) & 1,
-           (a >> 5) & 1,
-           (a >> 6) & 1,
-            a >> 7]
+        (a >> 1) & 1,
+        (a >> 2) & 1,
+        (a >> 3) & 1,
+        (a >> 4) & 1,
+        (a >> 5) & 1,
+        (a >> 6) & 1,
+        a >> 7
+    ]
 }
 
 function initialize(data) {
@@ -80,24 +81,24 @@ function outputItem(el, fmts, byteAmt, text) {
     li.appendChild(content)
     if (fmts.indexOf('hex') >= 0)
         content.innerHTML +=
-            '<code><grayed>0x</grayed>' +
-                hexify(item) +
-            '</code>'
+        '<code><grayed>0x</grayed>' +
+        hexify(item) +
+        '</code>'
     if (fmts.indexOf('dec') >= 0)
         content.innerHTML +=
-            '<code>' +
-                uintify(item) +
-            '</code>'
+        '<code>' +
+        uintify(item) +
+        '</code>'
     if (fmts.indexOf('bin') >= 0)
         content.innerHTML +=
-            '<code><grayed>0b</grayed>' +
-                binify(item) +
-            '</code>'
+        '<code><grayed>0b</grayed>' +
+        binify(item) +
+        '</code>'
     if (fmts.indexOf('direct') >= 0) {
         supported_codepoints.push(String.fromCodePoint(uintify(item)))
         content.innerHTML +=
             '<code class="utf">' +
-                String.fromCodePoint(uintify(item)) +
+            String.fromCodePoint(uintify(item)) +
             '</code>'
     }
     li.innerHTML += text
@@ -120,15 +121,17 @@ function loadHashTable(el, size) {
     for (let i = 0; i < size; i++) {
         if (i)
             outputSection(el, false)
-      let hash_value =
-        outputItem(el, ['hex'], 1, 'Value', 'Hash Value.')
-      let offset_table_size =
-        outputItem(el, ['hex'], 1, 'Offset-Table-Size', 'Offset Table Size.')
-      let offset_table_offset =
-        outputItem(el, ['hex', 'dec'], 2, 'Offset-Table-Offset', 'Offset location.')
-        data.push({'hash':   hash_value,
-                   'size':   offset_table_size,
-                   'offset': offset_table_offset})
+        let hash_value =
+            outputItem(el, ['hex'], 1, 'Value', 'Hash Value.')
+        let offset_table_size =
+            outputItem(el, ['hex'], 1, 'Offset-Table-Size', 'Offset Table Size.')
+        let offset_table_offset =
+            outputItem(el, ['hex', 'dec'], 2, 'Offset-Table-Offset', 'Offset location.')
+        data.push({
+            'hash': hash_value,
+            'size': offset_table_size,
+            'offset': offset_table_offset
+        })
     }
     return data
 }
@@ -146,17 +149,17 @@ function loadOffsetTables(el, offset_table_info, codepoint_bytes, features) {
         el.appendChild(document.createTextNode(JSON.stringify(i)))
         el.appendChild(document.createElement('br'))
         el.appendChild(document.createTextNode(
-            '∆=' + (offset-offset_table_offset - i['offset'])))
+            '∆=' + (offset - offset_table_offset - i['offset'])))
         // console.log(i)
-        while ((offset-offset_table_offset - i['offset']) < 0) {
+        while ((offset - offset_table_offset - i['offset']) < 0) {
             outputItem(el, ['hex'], 1, 'Unused!?', 'Discarded while getting to correct offset.')
         }
         for (let j = 0; j < i['size']; j++) {
-          let codepoint =
-            outputItem(el, ['hex', 'dec', 'direct'], codepoint_bytes, 'Codepoint', 'Unicode char.')
-          let charOffset =
-            outputItem(el, ['hex'], features & 1 ? 2 : 4, 'Offset', 'Data offset location.')
-            out.push({'codepoint': codepoint, 'offset': charOffset})
+            let codepoint =
+                outputItem(el, ['hex', 'dec', 'direct'], codepoint_bytes, 'Codepoint', 'Unicode char.')
+            let charOffset =
+                outputItem(el, ['hex'], features & 1 ? 2 : 4, 'Offset', 'Data offset location.')
+            out.push({ 'codepoint': codepoint, 'offset': charOffset })
         }
     }
     return out
@@ -178,19 +181,19 @@ function loadGlyphTable(el, glyph_table_info, codepoint_bytes, features) {
         code.innerText = String.fromCodePoint(i.codepoint)
         el.appendChild(code)
         console.log(offset, glyph_table_offset, i['offset'])
-        while ((offset-glyph_table_offset - i['offset']) < 0) {
+        while ((offset - glyph_table_offset - i['offset']) < 0) {
             outputItem(el, ['hex'], 4, 'Unused!?', 'Discarded while getting to correct offset.')
         }
-      let bitmapWidth =
-        outputItem(el, ['dec'], 1, 'Bmp-Width')
-      let bitmapHeight =
-        outputItem(el, ['dec'], 1, 'Bmp-Height')
-      let offsetLeft =
-        outputItem(el, ['dec'], 1, 'Offset-Left')
-      let offsetTop =
-        outputItem(el, ['dec'], 1, 'Offset-Top')
-      let horizontalAdvance =
-        outputItem(el, ['dec'], 1, 'Horiz-Advance')
+        let bitmapWidth =
+            outputItem(el, ['dec'], 1, 'Bmp-Width')
+        let bitmapHeight =
+            outputItem(el, ['dec'], 1, 'Bmp-Height')
+        let offsetLeft =
+            outputItem(el, ['dec'], 1, 'Offset-Left')
+        let offsetTop =
+            outputItem(el, ['dec'], 1, 'Offset-Top')
+        let horizontalAdvance =
+            outputItem(el, ['dec'], 1, 'Horiz-Advance')
         // outputItem(el, ['hex'], 3, '')
         if (features & 2) { // use rle4
 
@@ -208,7 +211,7 @@ function loadGlyphTable(el, glyph_table_info, codepoint_bytes, features) {
                 code += '<tr>'
                 for (let j = 0; j < bitmapWidth; j++) {
                     code += '<td class="' +
-                            (buffer.shift() ? 'l' : 'd') + '">'
+                        (buffer.shift() ? 'l' : 'd') + '">'
                     code += '</td>'
                 }
                 code += '</tr>'
@@ -229,38 +232,38 @@ function loadFile(f, el) {
     f = new Uint8Array(f);
     initialize(f)
 
-  let version =
-    outputItem(el, ['hex'], 1, 'Version', 'Font version.')
+    let version =
+        outputItem(el, ['hex'], 1, 'Version', 'Font version.')
     outputItem(el, ['dec'], 1, 'Max-Height', 'Line height.')
-  let glyph_amount =
-    outputItem(el, ['dec'], 2, 'Glyph-Amt', 'The total amount of glyphs encoded.')
+    let glyph_amount =
+        outputItem(el, ['dec'], 2, 'Glyph-Amt', 'The total amount of glyphs encoded.')
     outputItem(el, ['hex'], 2, 'Wildcard-Codepoint', 'The codepoint to use when a character isn\'t found.')
-  let hash_table_size =
-    outputItem(el, ['hex'], 1, 'Hash-Table-Size', 'Total size of the hash table.')
-  let codepoint_bytes =
-    outputItem(el, ['hex'], 1, 'Codepoint-Bytes', 'Size of a codepoint in the offset_table')
+    let hash_table_size =
+        outputItem(el, ['hex'], 1, 'Hash-Table-Size', 'Total size of the hash table.')
+    let codepoint_bytes =
+        outputItem(el, ['hex'], 1, 'Codepoint-Bytes', 'Size of a codepoint in the offset_table')
     let features = 0 // default u32 offsets in v2/v1
     if (version >= 3) {
         outputItem(el, ['hex'], 1, 'Size')
-      features =
-        outputItem(el, ['bin'], 1, 'Features [0b1 | 0: u32; 1: u16 /// 0b10 | bitmapped; 1: rle4]')
+        features =
+            outputItem(el, ['bin'], 1, 'Features [0b1 | 0: u32; 1: u16 /// 0b10 | bitmapped; 1: rle4]')
     }
     if (glyph_amount > 1000) {
         return
     }
-  let offset_table_info =
-    loadHashTable(el, hash_table_size)
-  let glyph_table_info =
-    loadOffsetTables(el, offset_table_info, codepoint_bytes, features)
+    let offset_table_info =
+        loadHashTable(el, hash_table_size)
+    let glyph_table_info =
+        loadOffsetTables(el, offset_table_info, codepoint_bytes, features)
     loadGlyphTable(el, glyph_table_info, features)
 
     let li = document.createElement('li')
-  let metrics_info =
-    outputSection(el, true, 'chars')
+    let metrics_info =
+        outputSection(el, true, 'chars')
     li.innerHTML = "Chars: " + supported_codepoints
-                                   .sort()
-                                   .map(a => '<code>' + a + '</code>')
-                                   .join(', ')
+        .sort()
+        .map(a => '<code>' + a + '</code>')
+        .join(', ')
     el.appendChild(li)
 
     setTimeout(() => window.scrollTo(0, document.querySelector('#chars').offsetTop - 100), 100)
@@ -283,7 +286,8 @@ dropEl.addEventListener('drop', e => {
             name = e.dataTransfer.files[0].name
         reader.readAsArrayBuffer(e.dataTransfer.files[0])
         reader.addEventListener('load',
-            () => {console.log(btoa(reader.result)); loadFile(reader.result, list)})
+            () => { console.log(btoa(reader.result));
+                loadFile(reader.result, list) })
     }
 })
 
